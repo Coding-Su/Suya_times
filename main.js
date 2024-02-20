7bc5f3a331ef4076a758b399c5b2e3ecd12022d9
Suya Kim <kkrladydtn@gmail.com>
2024-02-21 03:20:57 +0900

    찐막!

--- main.js
+++ main.js
@@ -11,33 +11,33 @@ const getLatestNews = async () => {
   const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles;
-  // render();
+  render();
   console.log("aaa", response);
   console.log("bbb", data);  
   console.log("ccc", newsList);
 };
 
-// const render = () => {
-//   const newsHTML = newsList.map(
-//     (news)=>`<div class="row news">
-//     <div class="col-lg-4">
-//       <img class="news-img-size" src=${urlToImage}/>
-//     </div>
-//     <div class="col-lg-8">
-//       <h2>${news.title}</h2>
-//       <p>
-//         ${news.description}
-//       </p>
-//       <div>
-//         ${news.source.name} * ${news.publishedAt}
-//       </div>
-//     </div>
-//   </div>`
-//   ).join('');
+const render = () => {
+  const newsHTML = newsList.map(
+    (news)=>`<div class="row news">
+    <div class="col-lg-4">
+      <img class="news-img-size" src=${urlToImage}/>
+    </div>
+    <div class="col-lg-8">
+      <h2>${news.title}</h2>
+      <p>
+        ${news.description}
+      </p>
+      <div>
+        ${news.source.name} * ${news.publishedAt}
+      </div>
+    </div>
+  </div>`
+  ).join('');
   
-//   console.log("html", newsHTML); 
+  console.log("html", newsHTML); 
 
-//   document.getElementById("news-board").innerHTML = newsHTML
-// }
+  document.getElementById("news-board").innerHTML = newsHTML
+}
 
 getLatestNews();
