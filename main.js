043e13d0e2bb88b17bd0c2591d2f9fe8b24828ff
Suya Kim <kkrladydtn@gmail.com>
2024-02-23 02:53:04 +0900

    good

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 const url2 = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
 const url3 = `https://suya-times.netlify.app/top-headlines`;
 let newsList = [];
+const menus = document.querySelectorAll(".menu button");
+
+menus.forEach((menu) => 
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+
+const searches = document.querySelectorAll(".search-button");
+
+searches.forEach((search) =>
+  search.addEventListener("click", (e) => getNewsByKeyword(e))
+); 
 
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
@@ -33,6 +44,38 @@ const getLatestNews = async () => {
   // console.log("bbb", data);  
   // console.log("ccc", newsList);
 };
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  // console.log("category", category);
+  const url = new URL(
+    `https://suya-times.netlify.app/top-headlines?category=${category}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();  
+  // console.log("Ddd", data);
+  newsList = data.articles;
+  render();
+};
+
+const getNewsByKeyword = async (event) => {
+  // const searchNews = function (el) {
+  //   console.log(el.previousElementSibling);
+  //   const keywordEl = el.previousElementSibling;
+  //   const keyword = keywordEl;
+  // };
+  
+  // console.log(event.target.previousElementSibling.value);
+  const keyword = event.target.previousElementSibling.value;
+  const url = new URL(
+    `https://suya-times.netlify.app/top-headlines?q=${keyword}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render()
+}
+
 
 const validateImageUrl = (imageUrl) => {
     // image객체 생성. 자바스크립트가 가지고 있는 인스턴스
@@ -103,3 +146,12 @@ const render = () => {
 }
 
 getLatestNews();
+
+// 1. 버튼에 클릭 이벤트 주기
+// 2. 카테고리별 뉴스 가져오기
+// 3. 관련 뉴스 보여주기 (render)
+
+
+// 1. 검색어를 받아온다.
+// 2. API호출한다.
+// 3. 응답이 돌아온다. 호출하는곳에 출력
